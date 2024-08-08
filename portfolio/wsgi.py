6fe61ef3606c5285638bf51e9f650a57fccbc678
MahmoudBayoumi58 <mahmoudtotti120@gmail.com>
2024-08-08 15:37:43 +0300

    Update wsgi file

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -8,11 +8,14 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
 application = get_wsgi_application()
+
+application = WhiteNoise(application, root=os.path.join(settings.BASE_DIR, 'staticfiles'))
 
 app = application
